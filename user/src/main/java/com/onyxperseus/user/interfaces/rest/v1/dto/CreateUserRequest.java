c727d40d30862c19f4c1773c2f387fff9f03802a
OnyxPerseus <nguyenminhquang2406@gmail.com>
2026-01-30 20:26:15 +0700

    fix: remove password field from CreateUserRequest DTO

--- user/src/main/java/com/onyxperseus/user/interfaces/rest/v1/dto/CreateUserRequest.java
+++ user/src/main/java/com/onyxperseus/user/interfaces/rest/v1/dto/CreateUserRequest.java
@@ -2,13 +2,10 @@ package com.onyxperseus.user.interfaces.rest.v1.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 public record CreateUserRequest(
     @NotBlank
     String userName,
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])[A-Za-z\\d@$!%*?&]{8,}")
-    String password,
     @Email
     String email,
     @NotBlank
